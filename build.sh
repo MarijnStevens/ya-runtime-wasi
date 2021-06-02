eec558a4becd424643505f7d0cd03d96f304707d
Marijn Stevens <marijnstevens@gmail.com>
2021-06-02 05:57:32 +0200

    Update script.

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
-# 
 # packages:  libssl-dev:aarch64 libglib2.0
 ARCH="aarch64"
 TARGET="$ARCH-unknown-linux-gnu"
 TARGET_DIR="./target/$TARGET/release/"
 
-echo -e "# build binary"
-#PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
-cargo build --release --target $TARGET && \
+echo Build for $ARCH
+PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
+cargo build --release --target $TARGET 
 
 #mkdir -p ../yagna-binaries/$ARCH/
 #\cp "$TARGET_DIR/gvmkit" ../yagna-binaries/$ARCH/

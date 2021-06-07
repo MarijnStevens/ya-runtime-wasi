23607ae45ef30154edb33b75b75d70bf9ba92dc8
Marijn Stevens <marijnstevens@gmail.com>
2021-06-07 02:00:08 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ TARGET_DIR="./target/$TARGET/release/"
 
 echo Build for $ARCH
 PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
-cargo build --release --target $TARGET 
+cargo build --release --target $TARGET && \
 
-\cp "$TARGET_DIR/ya-runtime-wasi" ../yagna-binaries/plugins/
+\cp "$TARGET_DIR/ya-runtime-wasi" ../yagna-binaries/plugins/ || exit 1 
